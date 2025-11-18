f918880243eeb6404fce303641d4bd23046e30e3
Ethan White <ethan.white1@stu.ocsb.ca>
2025-11-18 13:36:42 +0000

    finished all of assignment A

---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2025-11-13
+ * @fileoverview This program sums 3 constants.
+ */
+
+//Create variables
+const radius: number = 5.6;
+const pi: number = 3.14;
+
+//Calculate radius
+
+let Answer: number = pi * radius * radius
+
+//OUTPUT
+
+console.log("The area of a circle with a radius of " + radius + " cm is " + Answer + "cm²")
+
+console.log("\nDone")
